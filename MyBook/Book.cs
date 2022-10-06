478f6a9421ed6ac8f43ebd8d0edabf2ba9b425a4
xpanderex <razvan.maciovan@gmail.com>
2022-10-06 12:11:20 +0300

    Initial Commit

---
+++ MyBook/Book.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBook
+{
+    public class Book
+    {
+        public string Name { get; set; }
+        public List<string> paragraphs = new List<string>();
+        public List<string> images = new List<string>();
+        public List<string> tables = new List<string>();
+
+        public Book(string name)
+        {
+            this.Name = name;
+        }
+        public void createNewParagraph(string text)
+        {
+            this.paragraphs.Add(text);
+        }
+
+        public void createNewImage(string text)
+        {
+            this.images.Add(text);
+        }
+
+        public void createNewTable(string text)
+        {
+            this.tables.Add(text);
+        }
+
+        public void print()
+        {
+            Console.WriteLine($"{this.Name} paragraphs: ");
+            paragraphs.ForEach(paragraph => Console.WriteLine($"{paragraph}; "));
+            Console.WriteLine($"{this.Name} images: ");
+            images.ForEach(image => Console.WriteLine($"{image}; "));
+            Console.WriteLine($"{this.Name} paragraphs: ");
+            tables.ForEach(table => Console.WriteLine($"{table}; "));
+        }
+    }
+}
---
+++ MyBook/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book discoTitanic = new Book("Disco Titanic");
+            discoTitanic.createNewParagraph("Paragraph 1");
+            discoTitanic.createNewParagraph("Paragraph 2");
+            discoTitanic.createNewParagraph("Paragraph 3");
+            discoTitanic.createNewImage("Image 1");
+            discoTitanic.createNewParagraph("Paragraph 4");
+            discoTitanic.createNewTable("Table 1");
+            discoTitanic.createNewParagraph("Paragraph 5");
+
+            discoTitanic.print();
+            Console.ReadLine();
+
+        }
+    }
+}
